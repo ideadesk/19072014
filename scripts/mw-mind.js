e4478e243cf168d218413a7cb57b2dd7da48c5d4
Rahul <dev@rahul.se>
2014-09-30 05:08:24 +0100

    Autumn update - let the leaves fall
    Use Dyno Regular for all headings
    Add initial pages for Mind Works and Wellness Works
    Fix responsive layout for iPad and smaller screens
    Fix navbar menu for optimal screen space usage on collapse

---
+++ scripts/mw-mind.js
@@ -0,0 +1,129 @@
+/* ================================
+===  BACKGROUND SLIDER        ====
+================================= */
+$.vegas('slideshow', {
+  delay:4000,
+  backgrounds:[
+  { src:'images/pg/pg0.jpg', fade:1000 },
+    { src:'images/pg/pg1.jpg', fade:1000 }
+  ]
+});
+      
+   
+/* =================================
+   LOADER                     
+=================================== */
+// makes sure the whole site is loaded
+jQuery(window).load(function() {
+        // will first fade out the loading animation
+	jQuery(".status").fadeOut();
+        // will fade out the whole DIV that covers the website.
+	jQuery(".preloader").delay(1000).fadeOut("slow");
+})
+
+
+/* =================================
+===  Bootstrap Fix              ====
+=================================== */
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
+
+
+
+
+
+/* =================================
+===  WOW ANIMATION             ====
+=================================== */
+
+new WOW().init();
+
+
+  
+  /*=================================
+===  OWL CROUSEL               ====
+===================================*/
+   $(document).ready(function() {
+  var owl = $("#client-feedbacks");
+  owl.owlCarousel({
+      items : 3, //10 items above 1000px browser width
+      itemsDesktop : [1000,2], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,1], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
+      autoPlay : false,
+      stopOnHover: true,
+      rewindSpeed:1000,
+      itemsScaleUp: true,
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  });
+});
+
+
+
+  /* ================================
+===  PROJECT LOADING           ====
+================================= */
+
+jQuery(document).ready(function($) {
+    $('.more').on('click', function(event) {
+        event.preventDefault();
+
+        var href = $(this).attr('href') + ' .single-project',
+            portfolioList = $('#portfolio-list'),
+            content = $('#loaded-content');
+
+        portfolioList.animate({'marginLeft':'-120%'},{duration:400,queue:false});
+        portfolioList.fadeOut(400);
+        setTimeout(function(){ $('#loader').show(); },400);
+        setTimeout(function(){
+            content.load(href, function() {
+                $('#loaded-content meta').remove();
+                $('#loader').hide();
+                content.fadeIn(600);
+                $('#back-button').fadeIn(600);
+            });
+        },800);
+
+    });
+
+    $('#back-button').on('click', function(event) {
+        event.preventDefault();
+
+        var portfolioList = $('#portfolio-list')
+            content = $('#loaded-content');
+
+        content.fadeOut(400);
+        $('#back-button').fadeOut(400);
+        setTimeout(function(){
+            portfolioList.animate({'marginLeft':'0'},{duration:400,queue:false});
+            portfolioList.fadeIn(600);
+        },800);
+    });
+});
+ 
+
+
+
+ /* ================================
+===  PARALLAX                  ====
+================================= */
+$(document).ready(function(){
+  var $window = $(window);
+  $('div[data-type="background"], header[data-type="background"], section[data-type="background"]').each(function(){
+    var $bgobj = $(this);
+    $(window).scroll(function() {
+      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+      var coords = '50% '+ yPos + 'px';
+      $bgobj.css({ 
+        backgroundPosition: coords 
+      });
+    });
+  });
+});
